864506ac6810d63c47af09bfbc3e093585247050
Dmitry Kutin <dmitrykutin@outlook.com>
2020-01-28 22:28:26 -0500

    Release 1.0

--- constants.php
+++ constants.php
@@ -1,12 +1,14 @@
 <?php
 
+/*
+*   Configurable Constants
+*/
 
-// User specific values to be configed before run
-define('CONSUMER_KEY', '');
-define('CONSUMER_SECRET', '');
-define('LEAGUE_KEY', 'nba.l.');
-define('TEAM_ID', '');
-define('AUTH_ENDPOINT', 'https://api.login.yahoo.com/oauth2/get_token');
+define('CONSUMER_KEY', 'dj0yJmk9dWVMT3V2MU9SZGUzJmQ9WVdrOVJIWklVM3BMTXpBbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWVm');
+define('CONSUMER_SECRET', '42e262e98287f3fca465fa227b2344d62a140971');
+define('LEAGUE_KEY', 'nba.l.128774');
+define('TEAM_ID', '4');
+define('FREE_AGENTS_MAX', 50);
 static $scored_stats = [
     '12' => 1,      //PTS
     '15' => 1.2,    //REB
@@ -15,3 +17,10 @@ static $scored_stats = [
     '18' => 3,      //STL
     '19' => -1,     //TO
 ];
+
+/*
+ *  Non-Configurable Constants
+ */
+
+define('AUTH_ENDPOINT', 'https://api.login.yahoo.com/oauth2/get_token');
+
