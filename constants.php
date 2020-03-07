06fe3e6b03a0c19b91a269c70fb12286d03b8829
Dmitry Kutin <dmitrykutin@outlook.com>
2020-03-07 15:46:52 -0500

    Rewrite algorithm to include all averages and generate a csv of suggestions (#6)

--- constants.php
+++ constants.php
@@ -30,4 +30,8 @@ define('TMP_AUTH_DIR', __DIR__ . '/tmp/auth/');
 define('TMP_DATA_DIR', __DIR__ . '/tmp/data/');
 define('TMP_DATA_PLAYERS_DIR', __DIR__ . '/tmp/data/players/');
 
+// Define Stat Types
+define('ONE_WEEK_AVG', 1);
+define('TWO_WEEK_AVG', 2);
+define('ONE_MO_AVG', 4);
 
--- pi.php
+++ pi.php
@@ -1,8 +1,12 @@
 <?php
 
 include(__DIR__ . '/ServiceCall.php');
+include (__DIR__ . '/Analytics.php');
 
 $request = new ServiceCall();
+$analytics = new Analytics();
 
 $request->getRosterStats();
 $request->getFreeAgentsStats();
+$analytics->generateReport();
+
